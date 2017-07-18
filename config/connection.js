45ffcac72d198afa8df48ad4e3f79b8ea784b2e9
Brian K <karabinchak.brian@gmail.com>
2017-07-18 11:45:10 -0400

    changed connection details

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
 //Dependencies
 var Sequelize = require('sequelize');
+var mysql = require('mysql');
 
 //Creates MySQL connection using Sequelize
-var sequelize = new Sequelize('burgers_db', 'root', 'Bourassa13!bk', {
-	host: 'localhost',
-	dialect: 'mysql',
-	pool: {
-		max: 5,
-		min: 0,
-		idle: 10000
-	}
-});
+if (process.env.JAWSDB_URL) {
+	sequelize = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	var sequelize = new Sequelize('burgers_db', 'root', 'Bourassa13!bk', {
+		host: 'localhost',
+		dialect: 'mysql',
+		pool: {
+			max: 5,
+			min: 0,
+			idle: 10000
+		}
+	});	
+}
 
 //Export connection for other files
 module.exports = sequelize;
